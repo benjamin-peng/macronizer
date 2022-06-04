3d44debed5b871a16a5325b0c50ab6baee6e8d8b
Benjamin Peng <benjamin.peng@duke.edu>
2022-06-03 23:34:46 -0400

    implement api request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [word, setWord] = useState('dico');
+  const endpoint = 'https://en.wiktionary.org/w/api.php?';
+  const params = 'action=query' + '&prop=extracts&titles=' + word + '&format=json&origin=*';
+  const macronize = () => {
+    fetch(endpoint + params, {
+      method: 'GET'
+    })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      //console.log(data.query.pages[188276].extract);
+      var page = document.createElement('html');
+      page.innerHTML = data.query.pages[188276].extract;
+      //console.log(page.getElementsByClassName('Latn headword'));
+      for (let i = 0; i < page.getElementsByClassName('Latn headword').length; i++) {
+        if (page.getElementsByClassName('Latn headword')[i].lang == 'la') {
+          console.log('asdf');
+        }
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Macronizer</p>
+      <button onClick={macronize}>macronize</button>
     </div>
   );
 }
