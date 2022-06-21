0ed10acd6ac3411905ad8d7f308f9577ee29b091
Benjamin Peng <benjamin.peng@duke.edu>
2022-06-20 23:06:18 -0400

    create dropdown functionality on hover

---
+++ src/App/Dropup/index.js
@@ -0,0 +1,9 @@
+const Dropup = () => {
+    return (
+        <div className="dropup">
+            <p>testsettset</p>
+        </div>
+    );
+}
+ 
+export default Dropup;
--- src/App/MacronizedWord/index.js
+++ src/App/MacronizedWord/index.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
+import Dropup from '../Dropup';
 
 const MacronizedWord = ({wordList}) => {
 
     const [color, setColor] = useState('white'); 
-    const [wordIdx, setWordIdx] = useState(0); 
+    const [wordIdx, setWordIdx] = useState(0);
+    const [hover, setHover] = useState(false); 
 
     useEffect(() => {
         if (wordList.length > 1) setColor('#cffaf2');
@@ -21,10 +23,15 @@ const MacronizedWord = ({wordList}) => {
 
     return (  
         <div className="macronized-word" onClick={loopIdx} style={{backgroundColor: color}} 
-        onMouseEnter={() => {setColor('beige')}} onMouseLeave={() => {
+        onMouseEnter={() => {
+            setColor('beige');
+            setHover(true);
+        }} onMouseLeave={() => {
             if (wordList.length == 1) setColor('white');
             else setColor('#cffaf2');
+            setHover(false);
             }}>
+            {hover && <Dropup></Dropup>}
             <p id="output">{wordList[wordIdx]}</p>
         </div>
     );
